f32b6c1752ea8d69056f4221a12a506937ee3778
天天 <2757336070@qq.com>
2020-12-21 14:59:04 +0800

    用户登录-创建页面组件并配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,14 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
+// const Login = () => {import("@/views/login")};
+import Login from "@/views/login"
 const routes = [
-
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  }
 ]
 
 const router = new VueRouter({
